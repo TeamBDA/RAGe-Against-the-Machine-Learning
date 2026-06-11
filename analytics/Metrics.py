10d287c495647e45ad118991d87da491f761fc68
Anton-Ilyk <anton.ilyk12@gmail.com>
2026-06-11 23:29:31 +0100

    - 11/06/26 Antons` commit  - Fixed comments style  - Merged Speech2Text_v2.py functionallity into original file  - Added description for bits like this "if temp_rec_len == 0 or temp_rec_len < len(row['transcript'].split()):"  - bot_speaker and top_speaker functions merged into one  - created universal reader function with yield functionality, so can work with very big files  - added function which allows to create report csv file

--- analytics/Metrics.py
+++ analytics/Metrics.py
@@ -4,93 +4,98 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  # project root
 TRANSCRIPTIONS_DIR = os.path.join(BASE_DIR, "data")
 
-#Returns total amount of words per person
-#Not limited to 5 at the moment, as we have just 5 people in our team, but can be limited
+# Reading file and returning row by row, not everything together
+# Especially important for big CSV files
+def read_file(filename):
+    filepath = f"{TRANSCRIPTIONS_DIR}/{filename}"
+
+    with open(filepath, newline="") as csvfile:
+        reader = csv.DictReader(csvfile)
+
+        for row in reader:
+            yield row
+
+# Returns total amount of words per person
+# Not limited to 5 at the moment, as we have just 5 people in our team, but can be limited
 def total_words_num(filename):
     stats = {}
-    with open(f"{TRANSCRIPTIONS_DIR}/{filename}", newline='') as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            temp_speaker = row['speaker']
-            current_rec_len = stats.setdefault(temp_speaker, 0)
-            stats.update({temp_speaker: len(row['transcript'].split()) + current_rec_len})
+
+    for row in read_file(filename):
+        temp_speaker = row['speaker']
+        current_rec_len = stats.setdefault(temp_speaker, 0)
+        stats.update({temp_speaker: len(row['transcript'].split()) + current_rec_len})
 
     speaker = dict(sorted(stats.items(), key=lambda x: x[1], reverse=True))
     return speaker
 
-#Returns TOP speaker name and amount of words
-def top_speaker(filename):
+# Returns speaker with the most/least words spoken
+def ml_speaker(filename, mode):
     stats = {}
     temp_speaker = ""
     temp_rec_len = 0
-    with open(f"{TRANSCRIPTIONS_DIR}/{filename}", newline='') as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            if temp_rec_len == 0 or temp_rec_len < len(row['transcript'].split()):
-                temp_rec_len = len(row['transcript'].split())
-                temp_speaker = row['speaker']
 
-        if temp_speaker:
-            stats[temp_speaker] = temp_rec_len
+    for row in read_file(filename):
 
-    return stats
+        # Temporary length(temp_rec_len) == 0 -> in case when just function starting first time and temporary length == 0, so we save first value
+        if temp_rec_len == 0:
+            temp_rec_len = len(row['transcript'].split())
+            temp_speaker = row['speaker']
 
-#Returns BOTTOM speaker name and amount of words in meeting
-def bot_speaker(filename):
-    stats = {}
-    temp_speaker = ""
-    temp_rec_len = 0
-    with open(f"{TRANSCRIPTIONS_DIR}/{filename}", newline='') as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            if temp_rec_len == 0 or temp_rec_len > len(row['transcript'].split()):
-                temp_rec_len = len(row['transcript'].split())
-                temp_speaker = row['speaker']
+        # If temp_rec_len < len(row['transcript'].split()) -> checking if temporary length which was saved in the previous step < than current one
+        # If it's true -> we have a speaker with new highest amount of words per recording, so we need to save it in our stats dictionary
+        if mode == "most" and temp_rec_len < len(row['transcript'].split()):
+            temp_rec_len = len(row['transcript'].split())
+            temp_speaker = row['speaker']
 
-        if temp_speaker:
-            stats[temp_speaker] = temp_rec_len
+        # If temp_rec_len > len(row['transcript'].split()) -> checking if temporary length which was saved in the previous step > than current one
+        # If it's true -> we have a speaker with new lowest amount of words per recording, so we need to save it in our stats dictionary
+        elif mode == "least" and temp_rec_len > len(row['transcript'].split()):
+            temp_rec_len = len(row['transcript'].split())
+            temp_speaker = row['speaker']
+
+    # If temp_speaker not empty -> populate stats from temporary variables(name and amount of words)
+    if temp_speaker:
+        stats[temp_speaker] = temp_rec_len
 
     return stats
 
-#Returns total speaking time for each person involved in the meeting in seconds
-#Not limited to 5 at the moment, as we have just 5 people in our team, but can be limited
+# Returns total speaking time for each person involved in the meeting in seconds
+# Not limited to 5 at the moment, as we have just 5 people in our team, but can be limited
 def speakers_total_time(filename):
     stats = {}
-    with open(f"{TRANSCRIPTIONS_DIR}/{filename}", newline='') as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            temp_speaker = row['speaker']
-            current_rec_len = stats.setdefault(temp_speaker, 0)
-            stats.update({temp_speaker: round(float(row['total_speaking_time_seconds']) + current_rec_len, 2) })
+
+    for row in read_file(filename):
+        temp_speaker = row['speaker']
+        current_rec_len = stats.setdefault(temp_speaker, 0)
+        stats.update({temp_speaker: round(float(row['total_speaking_time_seconds']) + current_rec_len, 2) })
 
     speaker = dict(sorted(stats.items(), key=lambda x: x[1], reverse=True))
     return speaker
 
-#Returns total meeting speaking time in seconds
+# Returns total meeting speaking time in seconds
 def meeting_total_time(filename):
     time = 0.0
-    with open(f"{TRANSCRIPTIONS_DIR}/{filename}", newline='') as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            time += float(row['total_speaking_time_seconds'])
+
+    for row in read_file(filename):
+        time += float(row['total_speaking_time_seconds'])
 
     return round(time, 2)
 
-#Returns average speaking(recording) time per person in seconds
+# Returns average speaking(recording) time per person in seconds
 def average_time_per_speaker(filename):
     stats = {}
     records_per_speaker = {}
-    with open(f"{TRANSCRIPTIONS_DIR}/{filename}", newline='') as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            temp_speaker = row['speaker']
 
-            if temp_speaker not in records_per_speaker:
-                records_per_speaker[temp_speaker] = stats.setdefault(temp_speaker, 0)
 
-            records_per_speaker[temp_speaker] += 1
-            current_rec_len = stats.setdefault(temp_speaker, 0)
-            stats.update({temp_speaker: float(row['total_speaking_time_seconds']) + current_rec_len})
+    for row in read_file(filename):
+        temp_speaker = row['speaker']
+
+        if temp_speaker not in records_per_speaker:
+            records_per_speaker[temp_speaker] = stats.setdefault(temp_speaker, 0)
+
+        records_per_speaker[temp_speaker] += 1
+        current_rec_len = stats.setdefault(temp_speaker, 0)
+        stats.update({temp_speaker: float(row['total_speaking_time_seconds']) + current_rec_len})
 
     average_stats = {}
 
@@ -101,7 +106,7 @@ def average_time_per_speaker(filename):
 
     return average_stats
 
-#Return average speech rate per person in seconds
+# Return average speech rate per person in seconds
 def average_speech_rate(filename):
     stats = speakers_total_time(filename)
     words_per_speaker = total_words_num(filename)
@@ -115,9 +120,52 @@ def average_speech_rate(filename):
 
     return average_stats
 
+# Creating metrics report using available function from this module
+def generate_report_csv(input_filename, output_filename):
+    output_path = f"{TRANSCRIPTIONS_DIR}/{output_filename}"
+
+    most_words = ml_speaker(input_filename, "most")
+    least_words = ml_speaker(input_filename, "least")
+    speaking_times = speakers_total_time(input_filename)
+    speech_rates = average_speech_rate(input_filename)
+
+    most_words_speaker = list(most_words.keys())[0]
+    most_words_count = list(most_words.values())[0]
+
+    least_words_speaker = list(least_words.keys())[0]
+    least_words_count = list(least_words.values())[0]
+
+    total_time = meeting_total_time(input_filename)
+    average_time = round(total_time / len(speaking_times), 2)
+
+    report = [
+        ["Metric", "Result"],
+        ["Most words", f"{most_words_speaker}, {most_words_count} words"],
+        ["Least words", f"{least_words_speaker}, {least_words_count} words"],
+        ["Total speaking time", f"{total_time} seconds"],
+        ["Average speaking time per speaker", f"{average_time} seconds"],
+    ]
+
+    for index, speaker in enumerate(speaking_times, start=1):
+        report.append([
+            f"{index} speaker by time",
+            f"{speaker}, {speaking_times[speaker]} seconds"
+        ])
+
+    for speaker in speech_rates:
+        report.append([
+            f"{speaker} average speech rate",
+            f"{speech_rates[speaker]} words/second"
+        ])
+
+    with open(output_path, "w", newline="") as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerows(report)
+
 
 ### TEST PART
 ### CAN CHECK ANY METHOD IN THIS FILE JUST USING CODE BELOW AND A NAME OF CSV FILE
-#if __name__ == '__main__':
-#    test = average_speech_rate("transcriptions.csv")
-#    print(test)
+# if __name__ == '__main__':
+    # generate_report_csv("transcriptions_metrics.csv", "report.csv")
+    # test = average_speech_rate("transcriptions_metrics.csv")
+    # print(test)
