cc46a4f8d6d9d8758ae2814c7b3d235ac0d67762
“Kevin” <printricity@gmail.com>
2026-06-19 13:20:00 +0100

    Added a wrapper for the logign function to allow it to be used and log exceptions/errors in the pipeline steps, also added elapsed time to track when a steps executes or fails

--- pipeline.py
+++ pipeline.py
@@ -12,6 +12,7 @@
 import os
 import logging
 import sys
+import time
 from sklearn.pipeline import Pipeline as SKPipeline
 from sklearn.preprocessing import FunctionTransformer
 from sklearn.base import BaseEstimator, TransformerMixin
@@ -68,29 +69,47 @@ def pipeline_logger():
 
 logger = pipeline_logger()
 
+# Now using a wrapper function to output start/input and output for logging
+def wrapper_with_logging(fnc):
+    # Now run wrapperlog fnc with try/catch to capture steps and any exceptions
+    def wrapper_log(X):
+        logger.info(f"Starting logging for step {fnc.__name__}")
+        start = time.time()
+        try:
+            result = logger.result(fnc(X))
+            elapsed_time = time.time - start
+            logger.info(f"Completed step {fnc.__name__} in {elapsed_time:.3f}s")
+            return 
+
+        except Exception as e:
+            elapsed_time = time.time - start
+            logger.error(f"Error in pipeline step {fnc.__name__}, in {elapsed_time:.3f}s: with err {e}")
+            raise
+    return wrapper_log
+
 # Define all pipeline steps to execute all functions with outputs feeding inputs (where possible)
 Pipeline = Pipeline([
-'process_speech', FunctionTransformer(main),
-'correct_transcription', FunctionTransformer(correct_text),
-'loaddt', FunctionTransformer(load_data),
-'question_flag', FunctionTransformer(add_question_flag),
-'add_words', FunctionTransformer(add_num_words),
-'add_chars', FunctionTransformer(add_text_size_chars),
-'add_speech', FunctionTransformer(add_speech_rate),
-'add_speaker_turn', FunctionTransformer(add_speaker_turn_id),
-'save_data', FunctionTransformer(save_enriched_data),
-'check_csv', FunctionTransformer(get_csv_checks),
-'check_dtype', FunctionTransformer(get_dtype_checks),
-'check_timestamp', FunctionTransformer(get_timestamp_checks),
-'fileread', FunctionTransformer(read_file),
-'retrieve_rows', FunctionTransformer(get_rows),
-'total_words', FunctionTransformer(total_words_num),
-'speaker', FunctionTransformer(ml_speaker_rec),
-'speaker_time', FunctionTransformer(ml_speaker_t),
-'speakers total time', FunctionTransformer(speakers_total_time),
-'check_meeting_total_time', FunctionTransformer(meeting_total_time),
-'compute_speak_avg_time', FunctionTransformer(average_time_per_speaker),
-'compute_speak_avg_meeting', FunctionTransformer(average_time_per_meeting),    
-'compute_rate_avg_time', FunctionTransformer(average_speech_rate),
-'generate_report', FunctionTransformer(generate_report_csv)
+'process_speech', FunctionTransformer(wrapper_with_logging(main)),
+'correct_transcription', FunctionTransformer(wrapper_with_logging(correct_text)),
+'loaddt', FunctionTransformer(wrapper_with_logging(load_data)),
+'question_flag', FunctionTransformer(wrapper_with_logging(add_question_flag)),
+'add_words', FunctionTransformer(wrapper_with_logging(add_num_words)),
+'add_chars', FunctionTransformer(wrapper_with_logging(add_text_size_chars)),
+'add_speech', FunctionTransformer(wrapper_with_logging(add_speech_rate)),
+'add_speaker_turn', FunctionTransformer(wrapper_with_logging(add_speaker_turn_id)),
+'save_data', FunctionTransformer(wrapper_with_logging(save_enriched_data)),
+'check_csv', FunctionTransformer(wrapper_with_logging(get_csv_checks)),
+'check_dtype', FunctionTransformer(wrapper_with_logging(get_dtype_checks)),
+'check_timestamp', FunctionTransformer(wrapper_with_logging(get_timestamp_checks)),
+'fileread', FunctionTransformer(wrapper_with_logging(read_file)),
+'retrieve_rows', FunctionTransformer(wrapper_with_logging(get_rows)),
+'total_words', FunctionTransformer(wrapper_with_logging(total_words_num)),
+'speaker', FunctionTransformer(wrapper_with_logging(ml_speaker_rec)),
+'speaker_time', FunctionTransformer(wrapper_with_logging(ml_speaker_t)),
+'speakers total time', FunctionTransformer(wrapper_with_logging(speakers_total_time)),
+'check_meeting_total_time', FunctionTransformer(wrapper_with_logging(meeting_total_time)),
+'compute_speak_avg_time', FunctionTransformer(wrapper_with_logging(average_time_per_speaker)),
+'compute_speak_avg_meeting', FunctionTransformer(wrapper_with_logging(average_time_per_meeting)),    
+'compute_rate_avg_time', FunctionTransformer(wrapper_with_logging(average_speech_rate)),
+'generate_report', FunctionTransformer(wrapper_with_logging(generate_report_csv))
 ])
